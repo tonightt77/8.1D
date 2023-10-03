818d5f8cefba1c5455290297a9e4e24696343d16
Qisheng HU <huqisheng86@gmail.com>
2023-10-03 19:00:43 +1100

    Added Mailgun

---
+++ netlify/functions/subscribe.js
@@ -0,0 +1,37 @@
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+	username: 'api',
+	key: process.env.MAILGUN_API_KEY,
+});
+
+exports.handler = async (event, context) => {
+    if (event.httpMethod !== "POST") {
+        return { statusCode: 405, body: "Method Not Allowed" };
+    }
+
+    const params = JSON.parse(event.body);
+    const email = params.email;
+
+    try {
+        await mg.messages.create('sandbox5a8030a5a8b4433ba5f769c3ea2c706e.mailgun.org', {
+            from: "Mailgun Sandbox <postmaster@sandbox5a8030a5a8b4433ba5f769c3ea2c706e.mailgun.org>",
+            to: [email],
+            subject: "Subscription Successful",
+            text: "You have successfully subscribed!",
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "You have successfully subscribed!" })
+        };
+    } catch (err) {
+        console.error(err);
+        return {
+            statusCode: 500,
+            body: "Error sending email."
+        };
+    }
+};
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,14 +1,46 @@
-import React from "react";
-import InputIconChild from './Input'
-import Button from './Button'
-import './Subscribe.css'
+import React, { useState } from "react";
+import InputIconChild from './Input';
+import Button from './Button';
+import './Subscribe.css';
 
 const Subscribe = () => {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/.netlify/functions/subscribe', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert(data.message);
+      } else {
+        alert('Error subscribing. Please try again.');
+      }
+    } catch (error) {
+      console.error('There was an error sending the request', error);
+    }
+  };
+
   return (
-    <div class="signup-block">
+    <div className="signup-block">
       <p>SIGN UP FOR OUR DAILY INSIDER</p>
-      <InputIconChild type="text"/>
-      <Button type="submit" />
+      <form onSubmit={handleSubmit}>
+        <InputIconChild 
+          type="text" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+        />
+        <Button type="submit" />
+      </form>
     </div>
   );
 };
